a03674fd62d6051ed88b757a407066577ea331c9
nsmseifi <nsm.seifi@gmail.com>
2019-12-22 12:30:53 +0330

    full_name starts by name

--- db_migration/person.py
+++ db_migration/person.py
@@ -6,12 +6,13 @@ from user.models import Person
 def full_name_settling(db_session,username):
     logger.info(LogMsg.START,username)
     persons = db_session.query(Person).filter(Person.full_name==None).all()
-
+    res = list()
     for person in persons:
         if person.last_name is None:
             person.full_name = person.name
         elif person.name is None:
             person.full_name = person.last_name
         else:
-            person.full_name = '{} {}'.format(person.name or '',person.last_name or '')
-    return {'result':True}
+            person.full_name = '{} {}'.format(person.last_name ,person.name)
+        res.append(person.full_name)
+    return {'result':res}
